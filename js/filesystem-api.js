43e1722b363110466dda3b4f508b1c1d375cf956
Robert Curran <ro5635@gmail.com>
2020-08-01 13:22:12 +0100

    Revert Moves towards ES6 and Sane JavaScript
    This reverts commit 4bb634959b67c2caa29acce6a5dfffc5ace7602e.

--- js/filesystem-api.js
+++ js/filesystem-api.js
@@ -1,42 +1,42 @@
-import fetch from 'node-fetch';
 
-const fs = require('fs');
+const fs = require("fs")
 
-const pathChar = '\\';
+const pathChar = "\\"
 
-const root = `${process.env.APPDATA + pathChar}ygo-draft${pathChar}`;
-const cache = `${root + pathChar}cache${pathChar}`;
-const cardImages = `${cache + pathChar}card-images${pathChar}`;
+const root = process.env.APPDATA + pathChar + "ygo-draft" + pathChar
+const cache = root + pathChar + "cache" + pathChar
+const cardImages = cache + pathChar + "card-images" + pathChar
 
 const mkdirIfNotExistsSync = (path) => {
-  if (!fs.existsSync(path)) {
-    fs.mkdirSync(path);
-  }
-};
+	if (!fs.existsSync(path)) {
+    	fs.mkdirSync(path);
+	}
+}
 
-mkdirIfNotExistsSync(root);
-mkdirIfNotExistsSync(cache);
-mkdirIfNotExistsSync(cardImages);
+mkdirIfNotExistsSync(root)
+mkdirIfNotExistsSync(cache)
+mkdirIfNotExistsSync(cardImages)
 
-export const getCardImage = async (id, imageUrl) => {
-  const fileName = `${id}.jpg`;
-  const filePath = cardImages + pathChar + fileName;
+module.exports = {
+	getCardImage: async (id, imageUrl) => {
 
-  const res = await fetch(imageUrl);
-  await new Promise((resolve, reject) => {
-    const fileStream = fs.createWriteStream(filePath);
-    res.body.pipe(fileStream);
-    res.body.on('error', (err) => {
-      fileStream.close();
-      reject(err);
-    });
-    fileStream.on('finish', () => {
-      fileStream.close();
-      resolve();
-    });
-  });
+		const fileName = id + ".jpg"
+		const filePath = cardImages + pathChar + fileName
 
-  return imageUrl;
-};
+		const res = await fetch(imageUrl);
+		await new Promise((resolve, reject) => {
+			const fileStream = fs.createWriteStream(filePath);
+			res.body.pipe(fileStream);
+				res.body.on("error", (err) => {
+					fileStream.close();
+					reject(err);
+			});
+			fileStream.on("finish", function() {
+				fileStream.close();
+				resolve();
+			});
+		});
 
-export default { getCardImage };
+		return imageUrl
+	}
+}
