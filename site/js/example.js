260bbbe27a53ab87f87c2cd69bbddc74265f4bd9
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2020-08-01 20:24:48 +0100

    cleaned up file

--- site/js/example.js
+++ site/js/example.js
@@ -1,32 +1,31 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 	
-	var app = new Vue(
-		{
-		  el: '#app',
-		  data: {
-		    message: "Hello",
-				cards: [],
-				images: []
-		  },
-		  methods: {
-		  	openPack: async function() {
+	var app = new Vue({
+		el: '#app',
+		data: {
+		  message: "Hello",
+		  cards: [],
+		  images: []
+		},
+		methods: {
+			openPack: async function() {
 
-		  		await	window.openPack("LOB").then(tenCards => this.cards = tenCards)
+				await	window.openPack("LOB").then(tenCards => this.cards = tenCards)
 
-					const cardIds = this.cards.map(card => card.id)
+				const cardIds = this.cards.map(card => card.id)
 
-					this.images = []
-					for ( var i = 0 ; i < cardIds.length ; i++ )
-					{
-						const img = await window.getCardImage(cardIds[i])
-						this.images.push(img)
-					}
+				this.images = []
+				for ( var i = 0 ; i < cardIds.length ; i++ )
+				{
+					const img = await window.getCardImage(cardIds[i])
+					this.images.push(img)
+				}
 
-		  	}
-		  },
-			created: function() {
-				this.openPack()
-			}
-		})
+		  }
+		},
+		created: function() {
+			this.openPack()
+		}
+	})
 })
