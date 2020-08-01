c3fdc782da4ead21e30c7c0e410d900ad99b5f9f
hamzah khan <khanha3@aston.ac.uk>
2020-08-01 14:14:58 +0100

    Added Example of Vue.js

--- site/js/example.js
+++ site/js/example.js
@@ -1,12 +1,18 @@
 
-
-
 const exampleFunction = async() => {
 	const tenCards = await window.getTenCards()
 	console.log(tenCards)
 }
 
-
+// add vue code in here
 window.addEventListener('DOMContentLoaded', () => {
+//example Vue
+	var app = new Vue({
+	  el: '#app',
+	  data: {
+	    message: 'Hello Vue!'
+	  }
+	})
+
   exampleFunction()
 })
