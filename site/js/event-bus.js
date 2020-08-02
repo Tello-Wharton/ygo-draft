0d10f03510330989a74b6139cd829e83f4aae740
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2020-08-02 15:19:47 +0100

    cleanup

--- site/js/event-bus.js
+++ site/js/event-bus.js
@@ -1,12 +1,14 @@
 
 
 const eventBus = new Vue({
-	data: {},
-    created: function() {
-      this.$on("broadcast-message", (payload) => {
-        const { message } = payload;
-        console.log(`Vue handling broadcast-message with message:${message}`);
-        // this.message = message;
+  data: {},
+  created: function() {
+    this.$on("broadcast-message", (payload) => {
+
+      const { message } = payload;
+      console.log(`Vue handling broadcast-message with message:${message}`);
+      // this.message = message;
+      
     });
 	}
 });
