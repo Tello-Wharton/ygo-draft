19d350f0e815fc051254d3a2371dc764e69b8ef7
hamzah khan <khanha3@aston.ac.uk>
2020-08-02 18:03:26 +0100

    Updated main.js window size

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ let mainWindowGlobal;
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 720,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
